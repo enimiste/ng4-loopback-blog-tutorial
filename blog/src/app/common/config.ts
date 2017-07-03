f8256133e22322d75f17bc34142b787ef5eb5262
Nouni EL Bachir <nouni.elbachir@gmail.com>
2017-07-03 13:12:32 +0100

    Add central Config class

---
+++ blog/src/app/common/config.ts
@@ -0,0 +1,11 @@
+import {Headers} from '@angular/http';
+
+export class Config {
+    static serverUrl: string = 'http://localhost:3000/api/';
+
+    static headers: Headers = new Headers({
+        'Accept': 'application/json'
+    });
+
+    static currentUserKey: string = 'currentUser';
+}
